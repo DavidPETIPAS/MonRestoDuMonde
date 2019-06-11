e7252f05516913b22dd42bf24ca899dd429d0c88
David PETIPAS <david.petipas@gmail.com>
2019-06-11 22:27:25 +0200

    TabBar custom

---
+++ MonRestoDuMonde/Controleur/MonTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  MonTabBar.swift
+//  MonRestoDuMonde
+//
+//  Created by David PETIPAS on 11/06/2019.
+//  Copyright © 2019 David PETIPAS. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+    }
+}
