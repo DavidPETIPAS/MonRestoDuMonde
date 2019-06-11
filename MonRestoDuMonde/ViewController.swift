f877d5c6a08e5ab4732a1ccebc292a9a08c97488
David PETIPAS <david.petipas@gmail.com>
2019-06-11 21:48:42 +0200

    Initial Commit

---
+++ MonRestoDuMonde/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MonRestoDuMonde
+//
+//  Created by David PETIPAS on 11/06/2019.
+//  Copyright © 2019 David PETIPAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
